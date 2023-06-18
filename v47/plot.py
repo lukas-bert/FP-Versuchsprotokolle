2dbbe182706de3cb9ba6add44d2561e1d408656a
ttros <tom.troska@tu-dortmund.de>
2023-06-18 21:19:15 +0200

    v47 weiter

--- v47/plot.py
+++ v47/plot.py
@@ -1,21 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+# delta t / s, R / OHM, I / mA, U / V
+delta_t, R, I_, U = np.genfromtxt("content/data/data.txt", unpack=True)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+I = I_*1e-3
+#calculating Energy
+E = U*I*delta_t
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+#calculating T
+def T(R):
+    return 0.00134*R**2 + 2.296*R - 243.02
+
+np.savetxt(
+    "content/data/E.txt", np.array([E, T(R)+273.15, T(R)]).transpose(),
+     header = "E / J, T / K, T / °C",
+     fmt = ["%.1f", "%d", "%d"],
+     delimiter="\t\t"
+)
 
 # in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+# plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+# plt.savefig('build/plot.pdf')
