ad632c9d5185f6d5cd6353bc9d2e4ec2f180561a
Lukas Bertsch <lukas.bertsch@tu-dortmund.de>
2023-07-02 01:28:09 +0200

    AAAAAHHHHHHH :sos:

--- v44/plot.py
+++ v44/plot.py
@@ -15,7 +15,8 @@ theta, counts = np.genfromtxt("content/data/DScan.UXD", unpack = True)
 def model(t, t0, sigma, I0, B):
     return I0/(np.sqrt(2*np.pi*sigma**2)) * np.exp(-(t-t0)**2/(2*sigma**2)) + B
 
-params, pcov = op.curve_fit(model, theta, counts, p0 = [np.mean(theta), np.std(theta), 175e3, 0])
+bounds = [[-0.5, 0, 0, -0.001], [0.5, 0.5, 2e5, 1e4]]
+params, pcov = op.curve_fit(model, theta, counts, p0 = [np.mean(theta), np.std(theta), 175e3, 0], bounds=bounds)
 err = np.sqrt(np.diag(pcov))
 FWHM = 0.091
 I0 = params[2]
@@ -32,13 +33,13 @@ x = np.linspace(-0.5, 0.5, 1000)
 plt.errorbar(theta, counts, yerr= np.sqrt(counts), marker = "x", color = "black", lw = 0, elinewidth = 1, capsize= 1, label = "Messdaten", alpha = .7, ms = 5)
 plt.plot(x, model(x, *params), label = "Fit", c = "firebrick", lw = 1.5)
 plt.hlines(params[2]/(2*np.sqrt(2*np.pi*params[1]**2)) - params[3], xmin = -0.5, xmax = 0.5, color = "gray", ls = "dashed", label = r"$\frac{1}{2} \;I_0$")
-plt.vlines([params[0] - 1/2*FWHM, params[0] + 1/2*FWHM] , 0, 180e3, color = "cornflowerblue", label = f"FWHM: {FWHM}°")
+plt.vlines([params[0] - 1/2*FWHM, params[0] + 1/2*FWHM] , 0, 2e5, color = "cornflowerblue", label = f"FWHM: {FWHM}°")
 plt.legend()
 plt.xlim(-0.3, 0.3)
-plt.ylim(0, 1.8e5)
+plt.ylim(0, 2e5)
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0), useMathText=True)
-#plt.xlabel(r"$\alpha \mathbin{/} \unit{\degree}$")
-#plt.ylabel(r"$I \mathbin{/} \text{Hits}  \mathbin{/} \unit{\second}$")
+plt.xlabel(r"$\alpha \mathbin{/} \unit{\degree}$")
+plt.ylabel(r"$I \mathbin{/} \text{Hits}  \mathbin{/} \unit{\second}$")
 plt.grid()
 plt.tight_layout()
 #plt.show()
@@ -58,8 +59,8 @@ plt.vlines([z[25], z[28]], ymin=0, ymax= 2.1e5, ls = "dashed", color = "firebric
 plt.xlim(-1, 1)
 plt.ylim(0, 2e5)
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0), useMathText=True)
-#plt.xlabel(r"$z \mathbin{/} \unit{\milli\metre}$")
-#plt.ylabel(r"$I \mathbin{/} \text{Hits}  \mathbin{/} \unit{\second}$")
+plt.xlabel(r"$z \mathbin{/} \unit{\milli\metre}$")
+plt.ylabel(r"$I \mathbin{/} \text{Hits}  \mathbin{/} \unit{\second}$")
 plt.grid()
 plt.legend()
 plt.tight_layout()
@@ -85,8 +86,8 @@ plt.legend()
 plt.xlim(-0.5, 0.5)
 plt.ylim(0, 9e4)
 plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0), useMathText=True)
-#plt.xlabel(r"$\alpha \mathbin{/} \unit{\degree}$")
-#plt.ylabel(r"$I \mathbin{/} \text{Hits}  \mathbin{/} \unit{\second}$")
+plt.xlabel(r"$\alpha \mathbin{/} \unit{\degree}$")
+plt.ylabel(r"$I \mathbin{/} \text{Hits}  \mathbin{/} \unit{\second}$")
 plt.grid()
 plt.tight_layout()
 #plt.show()
@@ -108,14 +109,14 @@ t, c1 = np.genfromtxt("content/data/ReflectScan.UXD", unpack = True)
 t2, c2 = np.genfromtxt("content/data/DiffuserScan.UXD", unpack = True)
 
 plt.plot(t, c1, color = "cornflowerblue", label = "Reflektivität (unkorrigiert)")
-plt.plot(t2, c2, color = "firebrick", label = "Streureflektivität")
+plt.plot(t2, c2, color = "firebrick", label = "Streurintensität")
 plt.plot(t, c1-c2, label = "Differenz", ls = "dashed")
 
 plt.legend()
 plt.yscale("log")
 plt.xlim(0, 2.5)
-#plt.xlabel(r"$\alpha \mathbin{/} \unit{\degree}$")
-#plt.ylabel(r"$I \mathbin{/} \text{Hits}  \mathbin{/} \unit{\second}$")
+plt.xlabel(r"$\alpha \mathbin{/} \unit{\degree}$")
+plt.ylabel(r"$I \mathbin{/} \text{Hits}  \mathbin{/} \unit{\second}$")
 plt.tight_layout()
 #plt.show()
 
@@ -124,7 +125,7 @@ plt.close()
 
 ########################################################################
 
-a_c = 0.153 # ° (kritischer Winkel von Si)
+a_c = 0.223 # ° (kritischer Winkel von Polysterol)
 
 def r(alpha):
     return (a_c/(2*alpha))**4
@@ -159,13 +160,13 @@ plt.plot(t, R_c, label = "Reflektivität (korrigiert)", c = "cornflowerblue")
 plt.plot(x, r(x), label = "Fresnelreflektivität", color = "black")
 plt.plot(x2, [r(a_c), r(a_c)], color = "black")
 plt.plot(t[idx], R_c[idx], lw = 0, marker = "^", label = "Minima", c = "firebrick", ms = 5)
-#plt.vlines(a_c, 0, 10e3, label = r"$\alpha_c$ (Polysterol)", color = "deeppink")
+plt.vlines(a_c, 0, 10e3, label = r"$\alpha_c$ (Polysterol)", color = "deeppink")
 plt.legend()
 plt.yscale("log")
 plt.xlim(0, 2.5)
 plt.ylim(None, 10e3)
-#plt.xlabel(r"$\alpha \mathbin{/} \unit{\degree}$")
-#plt.ylabel(r"$R$ (a.u.)")
+plt.xlabel(r"$\alpha \mathbin{/} \unit{\degree}$")
+plt.ylabel(r"$R$ (a.u.)")
 plt.tight_layout()
 #plt.show()
 plt.savefig("build/Reflek2.pdf")
@@ -179,12 +180,12 @@ k = 2*np.pi/lam
 n1 = 1
 d1 = 0
 delta_Poly = 3.5e-6 # 1. Schicht Polysterol
-delta_Si = 6.5e-6 # 2. Schicht Silizium
+delta_Si = 7.6e-6 # 2. Schicht Silizium
 b_Poly = delta_Poly*0.025
 b_Si = delta_Si/200
 d_ = noms(d)
-sigma_Poly = 0
-sigma_Si = 0
+sigma_Poly = 1e-10
+sigma_Si = 1e-10
 params = [delta_Poly, delta_Si, b_Poly, b_Si, d_, sigma_Poly, sigma_Si] # Startwerte
 err = np.zeros(len(params))
 
@@ -192,9 +193,9 @@ def parratt(a, delta2, delta3, b2, b3, d2, sigma1, sigma2):
     n2 = 1.0 - delta2 + b2*1j
     n3 = 1.0 - delta3 + b3*1j
     a = np.deg2rad(a)
-    kd1 = k *  np.emath.sqrt(n1**2 - np.cos(a)**2)
-    kd2 = k * np.emath.sqrt(n2**2 - np.cos(a)**2)
-    kd3 = k * np.emath.sqrt(n3**2 - np.cos(a)**2)
+    kd1 = k *  np.sqrt(n1**2 - np.cos(a)**2)
+    kd2 = k * np.sqrt(n2**2 - np.cos(a)**2)
+    kd3 = k * np.sqrt(n3**2 - np.cos(a)**2)
 
     r12 = ((kd1 - kd2)/(kd1 + kd2))*np.exp(-2*kd1*kd2*sigma1**2)
     r23 = ((kd2 - kd3)/(kd2 + kd3))*np.exp(-2*kd2*kd3*sigma2**2)
@@ -206,9 +207,9 @@ def parratt(a, delta2, delta3, b2, b3, d2, sigma1, sigma2):
 
 # Fitbereich
 t_min = 0.3
-t_max = 1.2
+t_max = 1.3
 
-bounds = [0, 1e-4]
+bounds = ([5e-7, 5e-7, 1e-10, 1e-10, 1e-9, 0, 0], [5e-5, 5e-5, 1e-6, 1e-6, 1e-7, 1e-9, 1e-9])
 params, pcov = op.curve_fit(parratt, t[(t>t_min) * (t<t_max)], R_c[(t>t_min) * (t<t_max)], p0 = params, bounds = bounds)
 err = np.sqrt(np.diag(pcov))
 
@@ -229,17 +230,19 @@ print(f"alpha_c (Poly)  : {a_c_Poly:.4f} °")
 print(f"alpha_c (Si)    : {a_c_Si:.4f} °")
 print("-------------------------------------------------------")
 
+x = np.linspace(0, 2.5, 10000)
+
 plt.plot(t, R_c, label = "gemessene Reflektivität (korrigiert)", c = "cornflowerblue")
-plt.plot(t, parratt(t, *params), color = "firebrick", alpha = .8, label = "Parrattalgorithmus")
-#plt.vlines(0.153, 0, 10e3, label = r"$\alpha_c$ (Polysterol) = " + f"{a_c_Poly:.4f}°", color = "deeppink")
-#plt.vlines(0.174, 0, 10e3, label = r"$\alpha_c$ (Si) = " + f"{a_c_Si:.4f}°", color = "rebeccapurple")
+plt.plot(x, parratt(x, *params), color = "firebrick", alpha = .8, label = "Parrattalgorithmus")
+#plt.vlines(noms(a_c_Poly), 0, 10e3, label = r"$\alpha_c$ (Polysterol) = " + f"{a_c_Poly:.4f}°", color = "deeppink")
+#plt.vlines(noms(a_c_Si), 0, 10e3, label = r"$\alpha_c$ (Si) = " + f"{a_c_Si:.4f}°", color = "rebeccapurple")
 plt.legend()
 plt.yscale("log")
 plt.xlim(0, 2.5)
 plt.ylim(None, 10e3)
-#plt.xlabel(r"$\alpha \mathbin{/} \unit{\degree}$")
+plt.xlabel(r"$\alpha \mathbin{/} \unit{\degree}$")
 plt.ylabel(r"$R$ (a.u.)")
 plt.tight_layout()
-plt.show()
+#plt.show()
 plt.savefig("build/Reflek3.pdf")
 plt.close()
